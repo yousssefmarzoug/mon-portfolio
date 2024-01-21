4ac6836013b07fc74dfa86549ad446a92631043b
youssef marzoug <youssefmarzoug19@gmail.com>
2024-01-21 12:47:20 +0100

    test index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ModalProvider } from './context/modal-context';
 import { ThemeProvider } from './context/theme-context';
 import './index.css'
 
-
+//test 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
     <ThemeProvider>
